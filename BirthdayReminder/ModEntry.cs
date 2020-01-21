6c91d2527442f2deebf453d427fb50110a44c9d7
tihadot <timo.hardebusch@mailbox.org>
2020-01-21 07:53:27 +0100

    add initial mod files

---
+++ BirthdayReminder/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace BirthdayReminder
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method invoked when a new day starts.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnDayStarted(object sender, EventArgs e)
+        {
+            NPC bdayNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
+            string bdayMsg = "Today's birthday: " + bdayNPC.Name;
+            Game1.addHUDMessage(new HUDMessage(bdayMsg, 2));
+        }
+    }
+}
