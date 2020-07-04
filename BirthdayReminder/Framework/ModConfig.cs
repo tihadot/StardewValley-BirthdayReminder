d87a8c397b8b1c809d4bef0bb9ee992d1e616c51
tihadot <timo.hardebusch@mailbox.org>
2020-07-04 17:47:31 +0200

    add JSON configuration support

---
+++ BirthdayReminder/Framework/ModConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace BirthdayReminder.Framework
+{
+    internal class ModConfig
+    {
+        /*********
+        ** Accessors
+        *********/
+        /// <summary>Whether to show birthday reminders for the current day.</summary>
+        public bool ShowBirthdayToday { get; set; } = true;
+
+        /// <summary>Whether to show birthday reminders for the next day.</summary>
+        public bool ShowBirthdayTomorrow { get; set; } = true;
+    }
+}
--- BirthdayReminder/ModEntry.cs
+++ BirthdayReminder/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BirthdayReminder.Framework;
 using StardewModdingAPI;
 using StardewValley;
 
@@ -8,12 +9,20 @@ namespace BirthdayReminder
     public class ModEntry : Mod
     {
         /*********
+        ** Fields
+        *********/
+        /// <summary>The mod configuration.</summary>
+        private ModConfig Config;
+
+        /*********
         ** Public methods
         *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
+            Config = this.Helper.ReadConfig<ModConfig>();
+
             helper.Events.GameLoop.DayStarted += this.OnDayStarted;
         }
 
@@ -27,45 +36,51 @@ namespace BirthdayReminder
         {
 
             // Check for today's birthday
-            NPC bdayNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
-            if (bdayNPC != null)
+            if (Config.ShowBirthdayToday)
             {
-                bdayNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
-                string bdayMsg = Helper.Translation.Get("bday-today") + bdayNPC.getName();
-                Game1.addHUDMessage(new HUDMessage(bdayMsg, 2));
+                NPC bdayNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
+                if (bdayNPC != null)
+                {
+                    bdayNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth);
+                    string bdayMsg = Helper.Translation.Get("bday-today") + bdayNPC.getName();
+                    Game1.addHUDMessage(new HUDMessage(bdayMsg, 2));
+                }
             }
 
             // Check for tomorrow's birthday
-            NPC bdayTomorrowNPC;
-
-            // Tomorrow could be in another season. We have to take this into account.
-            if (Game1.dayOfMonth <= 27)
-                bdayTomorrowNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth + 1);
-            else
+            if (Config.ShowBirthdayTomorrow)
             {
-                string nextSeason = Game1.currentSeason;
-                switch (Game1.currentSeason)
+                NPC bdayTomorrowNPC;
+
+                // Tomorrow could be in another season. We have to take this into account.
+                if (Game1.dayOfMonth <= 27)
+                    bdayTomorrowNPC = Utility.getTodaysBirthdayNPC(Game1.currentSeason, Game1.dayOfMonth + 1);
+                else
                 {
-                    case "spring":
-                        nextSeason = "summer";
-                        break;
-                    case "summer":
-                        nextSeason = "fall";
-                        break;
-                    case "fall":
-                        nextSeason = "winter";
-                        break;
-                    case "winter":
-                        nextSeason = "spring";
-                        break;
+                    string nextSeason = Game1.currentSeason;
+                    switch (Game1.currentSeason)
+                    {
+                        case "spring":
+                            nextSeason = "summer";
+                            break;
+                        case "summer":
+                            nextSeason = "fall";
+                            break;
+                        case "fall":
+                            nextSeason = "winter";
+                            break;
+                        case "winter":
+                            nextSeason = "spring";
+                            break;
+                    }
+                    bdayTomorrowNPC = Utility.getTodaysBirthdayNPC(nextSeason, 1);
                 }
-                bdayTomorrowNPC = Utility.getTodaysBirthdayNPC(nextSeason, 1);
-            }
 
-            if (bdayTomorrowNPC != null)
-            {
-                string bdayTomorrowMsg = Helper.Translation.Get("bday-tomorrow") + bdayTomorrowNPC.getName();
-                Game1.addHUDMessage(new HUDMessage(bdayTomorrowMsg, 2));
+                if (bdayTomorrowNPC != null)
+                {
+                    string bdayTomorrowMsg = Helper.Translation.Get("bday-tomorrow") + bdayTomorrowNPC.getName();
+                    Game1.addHUDMessage(new HUDMessage(bdayTomorrowMsg, 2));
+                }
             }
         }
     }
